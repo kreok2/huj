15abd33ebb0bf2b8e372459abb467aa7b1cd5c5a
valera <demianvalerii2@gmail.com>
2023-03-07 19:12:24 +0100

    first commit

---
+++ prodamiljunedoroho/script.js
@@ -0,0 +1,38 @@
+     //"use strict";
+     //let number = 5;
+     //const leftBorderWith = 1;
+
+     //number == 10;
+     //console.log(number);
+
+     //const obj = {
+     //    name: "John ",
+   //      age: 25,
+ //        isMarrued: false
+
+     
+     //.console.log(obj.name);
+     //alert("hello")
+     //const result = confirm( "are you here?")
+     //console.log(   result);
+
+     //
+     // const answer = prompt( "вам есть 18?"," ");
+     //console.log( typeof(answer));
+    
+    // const answers = [  ];
+
+     //answers[0 ]= prompt('как ваше имя', "");
+     //answers[1 ]= prompt('как ваше фамилия', "");
+    // answers[2 ]= prompt('сколько вам лет', "");
+
+      // console.log( typeof(answers)) 
+
+//const category = "toys";
+
+//console.log( "https: //someurl.com/"+ category);
+
+
+ 
+//const user = "ivan";
+//alert(`пивет${user}
